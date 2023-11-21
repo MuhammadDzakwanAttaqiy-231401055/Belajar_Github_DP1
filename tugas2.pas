1de737e40933d1d8496016105403a458c55b409d
MuhammadDzakwanAttaqiy-231401055 <151598799+MuhammadDzakwanAttaqiy-231401055@users.noreply.github.com>
2023-11-21 10:56:10 +0700

    test upload
    coba coba

---
+++ tugas2.pas
@@ -0,0 +1,17 @@
+program tugas2;
+uses crt;
+begin
+clrscr;
+  writeln('Desember');
+  writeln('November');
+  writeln('Oktober');
+  writeln('September');
+  writeln('Agustus');
+  writeln('Juli');
+  writeln('Juni');
+  writeln('Mei');
+  writeln('April');
+  writeln('Maret');
+  writeln('Februari');
+  writeln('Januari');
+end.
